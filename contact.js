54e1729a0a144af2492d39d210754a848540de9e
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2022-03-18 12:05:40 +0100

    alert on submit + theme

--- contact.js
+++ contact.js
@@ -1,10 +1,40 @@
-const message =
-  "Votre message a bien été envoyé";
-
+/* alert submit */
 const form = document.querySelector("form");
+const show = document.querySelector("div");
+const hide = document.querySelector("span");
+
+
 if (form) {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    alert(message);
+    show.classList.add("show")
   });
 }
+
+if (hide) {
+  hide.addEventListener("click", function (event) {
+    event.preventDefault();
+    show.classList.remove("show")
+  })
+}
+
+
+/* theme dark/light*/
+const button = document.querySelector(".btn-toggle")
+const body = document.querySelector("body")
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "light") {
+  body.classList.add("light");
+}
+
+button.addEventListener("click", function () {
+  body.classList.toggle("light");
+
+  let theme = "dark";
+  if (body.classList.contains("light")) {
+    theme = "light";
+  }
+  localStorage.setItem("theme", theme);
+})

b6761fa5c66dd80f451356d44b5e4ab7c6875db0
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2022-03-23 12:27:47 +0100

    version sans les texte et filtre perso

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const burger = document.querySelector("#nav");
-const btn = document.querySelector('.btn-nav')
+const btn = document.querySelector(".btn-nav");
 
 if (btn) {
-    btn.addEventListener("click", function (event) {
-      event.preventDefault();
-      burger.classList.toggle("isOpen")
-    });
-  }
-
-  
-  
+  btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    burger.classList.toggle("isOpen");
+  });
+}
